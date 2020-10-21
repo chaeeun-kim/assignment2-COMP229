6ed150daa82f86254a4c7db0365dc6ab77f2d631
chaeeun kim <afafpo@naver.com>
2020-10-21 13:39:24 +0900

    updated db URI to work with MongoDB

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports =
 {
-"URI":"mongodb://localhost/contacts"
+//"URI":"mongodb://localhost/contacts"
+"URI":"mongodb+srv://chaeeun:7HM9TlRK7uFdSCrD@mongodbserver.yzusu.mongodb.net/contacts?retryWrites=true&w=majority"
 };
 module.exports.Secret="SomeSecret";
 //i can put the name of database instead of book_store
